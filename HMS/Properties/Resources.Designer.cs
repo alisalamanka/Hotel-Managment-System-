0813c122058fd4248836ec57560bd39757fc491d
Achaaib AbdelAli <aliachaaib90@gmail.com>
2026-09-02 20:55:09 +0100

    Revert 4 add main form feature (#5)
    * add main form buttons (#4)
    
    Co-authored-by: abdo salamanka <aliachaaib50@gmail.com>
    
    * Revert "add main form buttons (#4)"
    
    This reverts commit a7d64c84f94f8d6452796d700919b7be5f78150c.
    
    ---------
    
    Co-authored-by: abdo salamanka <aliachaaib50@gmail.com>

--- HMS/Properties/Resources.Designer.cs
+++ HMS/Properties/Resources.Designer.cs
@@ -143,12 +143,6 @@ namespace HMS.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap background {
-            get {
-                object obj = ResourceManager.GetObject("background", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
         
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
