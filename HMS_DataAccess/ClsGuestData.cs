7b2af9dee44184da2b8792e4924fab6a4bae38db
abdo salamanka <aliachaaib50@gmail.com>
2026-09-21 21:24:00 +0000

    Guest Data Layer Added

---
+++ HMS_DataAccess/ClsGuestData.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HMS_DataAccess
+{
+    public class ClsGuestData
+    {
+        public static bool GetGuestInfoByID(
+            int GuestID,
+            ref int PersonID,
+            ref bool IsActive,
+            ref Exception ErrorOccurred)
+        {
+            string conn = ConfigurationManager.AppSettings["ConnectionString"];
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(conn))
+                using (SqlCommand cmd = new SqlCommand(
+                    "SP_GetGuestInfoByID", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@GuestID", GuestID);
+
+                    cnn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            PersonID = (int)dr["PersonID"];
+                            IsActive = (bool)dr["IsActive"];
+
+                            return true;
+                        }
+
+                        return false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred = e;
+                return false;
+            }
+        }
+
+
+        public static bool GetGuestInfoByPersonID(
+            int PersonID,
+            ref int GuestID,
+            ref bool IsActive,
+            ref Exception ErrorOccurred)
+        {
+            string conn = ConfigurationManager.AppSettings["ConnectionString"];
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(conn))
+                using (SqlCommand cmd = new SqlCommand(
+                    "SP_GetGuestInfoByPersonID", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@PersonID", PersonID);
+
+                    cnn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            GuestID = (int)dr["GuestID"];
+                            IsActive = (bool)dr["IsActive"];
+
+                            return true;
+                        }
+
+                        return false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred = e;
+                return false;
+            }
+        }
+
+
+        public static int? AddNewGuest(
+            int PersonID,
+            bool IsActive,
+            ref Exception ErrorOccurred)
+        {
+            string conn = ConfigurationManager.AppSettings["ConnectionString"];
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(conn))
+                using (SqlCommand cmd = new SqlCommand(
+                    "SP_AddNewGuest", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@PersonID", PersonID);
+                    cmd.Parameters.AddWithValue("@IsActive", IsActive);
+
+                    cnn.Open();
+
+                    return Convert.ToInt32(cmd.ExecuteScalar());
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred = e;
+                return null;
+            }
+        }
+
+
+        public static bool UpdateGuest(
+            int GuestID,
+            int PersonID,
+
+            string FirstName,
+            string LastName,
+            DateTime DateOfBirth,
+            string Phone,
+            string Email,
+            string NationalNo,
+            int CountryID,
+            byte Gender,
+            string ImagePath,
+
+            bool IsActive,
+            ref Exception ErrorOccurred)
+        {
+            string conn = ConfigurationManager.AppSettings["ConnectionString"];
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(conn))
+                using (SqlCommand cmd = new SqlCommand(
+                    "SP_UpdateGuest", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@GuestID", GuestID);
+                    cmd.Parameters.AddWithValue("@PersonID", PersonID);
+
+                    cmd.Parameters.AddWithValue("@FirstName", FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", LastName);
+                    cmd.Parameters.AddWithValue("@DateOfBirth", DateOfBirth);
+                    cmd.Parameters.AddWithValue("@Phone", Phone);
+                    cmd.Parameters.AddWithValue("@Email", Email);
+                    cmd.Parameters.AddWithValue("@NationalNo", NationalNo);
+                    cmd.Parameters.AddWithValue("@CountryID", CountryID);
+                    cmd.Parameters.AddWithValue("@Gender", Gender);
+                    cmd.Parameters.AddWithValue("@ImagePath", ImagePath);
+
+                    cmd.Parameters.AddWithValue("@IsActive", IsActive);
+
+                    cnn.Open();
+
+                    int RowsAffected =
+                        Convert.ToInt32(cmd.ExecuteScalar());
+
+                    return RowsAffected > 0;
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred = e;
+                return false;
+            }
+        }
+
+
+        public static bool DeleteGuest(
+            int GuestID,
+            ref Exception ErrorOccurred)
+        {
+            string conn = ConfigurationManager.AppSettings["ConnectionString"];
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(conn))
+                using (SqlCommand cmd = new SqlCommand(
+                    "SP_DeleteGuest", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@GuestID", GuestID);
+
+                    cnn.Open();
+
+                    int RowsAffected =
+                        Convert.ToInt32(cmd.ExecuteScalar());
+
+                    return RowsAffected > 0;
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred = e;
+                return false;
+            }
+        }
+
+
+        public static DataTable GetGuestsList(
+            ref Exception ErrorOccurred)
+        {
+            string conn = ConfigurationManager.AppSettings["ConnectionString"];
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(conn))
+                using (SqlCommand cmd = new SqlCommand(
+                    "SP_GuestsList", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cnn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        DataTable dt = new DataTable();
+
+                        dt.Load(dr);
+
+                        return dt;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred = e;
+                return null;
+            }
+        }
+
+
+        public static bool GuestExistsByID(
+            int GuestID,
+            ref Exception ErrorOccurred)
+        {
+            string conn = ConfigurationManager.AppSettings["ConnectionString"];
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(conn))
+                using (SqlCommand cmd = new SqlCommand(
+                    "SP_GuestExistsByID", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@GuestID", GuestID);
+
+                    cnn.Open();
+
+                    return Convert.ToInt32(
+                        cmd.ExecuteScalar()) > 0;
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred = e;
+                return false;
+            }
+        }
+
+
+        public static bool GuestExistsByPersonID(
+            int PersonID,
+            ref Exception ErrorOccurred)
+        {
+            string conn = ConfigurationManager.AppSettings["ConnectionString"];
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(conn))
+                using (SqlCommand cmd = new SqlCommand(
+                    "SP_GuestExistsByPersonID", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@PersonID", PersonID);
+
+                    cnn.Open();
+
+                    return Convert.ToInt32(
+                        cmd.ExecuteScalar()) > 0;
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred = e;
+                return false;
+            }
+        }
+    }
+}
