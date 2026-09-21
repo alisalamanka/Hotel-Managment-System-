7df9a66a5bf28b1171d8b60aa773f40a25880042
abdo salamanka <aliachaaib50@gmail.com>
2026-09-21 21:42:29 +0000

    Guest Details User Controls Added

---
+++ HMS/Guests/Controls/ctrlGuestDetails.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace HMS.Guests.Controls
+{
+    partial class ctrlGuestDetails
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uCpersonCard1 = new HMS.People.Controls.UCpersonCard();
+            this.guna2GroupBox1 = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblGuestID = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.lblIsActive = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uCpersonCard1
+            // 
+            this.uCpersonCard1.BackColor = System.Drawing.Color.Gainsboro;
+            this.uCpersonCard1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uCpersonCard1.Location = new System.Drawing.Point(31, 21);
+            this.uCpersonCard1.Margin = new System.Windows.Forms.Padding(4);
+            this.uCpersonCard1.Name = "uCpersonCard1";
+            this.uCpersonCard1.Size = new System.Drawing.Size(930, 354);
+            this.uCpersonCard1.TabIndex = 0;
+            // 
+            // guna2GroupBox1
+            // 
+            this.guna2GroupBox1.BorderRadius = 17;
+            this.guna2GroupBox1.Controls.Add(this.lblIsActive);
+            this.guna2GroupBox1.Controls.Add(this.lblGuestID);
+            this.guna2GroupBox1.Controls.Add(this.guna2HtmlLabel2);
+            this.guna2GroupBox1.Controls.Add(this.guna2HtmlLabel1);
+            this.guna2GroupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2GroupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.guna2GroupBox1.Location = new System.Drawing.Point(31, 376);
+            this.guna2GroupBox1.Name = "guna2GroupBox1";
+            this.guna2GroupBox1.Size = new System.Drawing.Size(916, 129);
+            this.guna2GroupBox1.TabIndex = 1;
+            this.guna2GroupBox1.Text = "Guest Information";
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(34, 71);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(75, 23);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "Guest ID : ";
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(486, 71);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(75, 23);
+            this.guna2HtmlLabel2.TabIndex = 1;
+            this.guna2HtmlLabel2.Text = "Is Active : ";
+            // 
+            // lblGuestID
+            // 
+            this.lblGuestID.BackColor = System.Drawing.Color.Transparent;
+            this.lblGuestID.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuestID.ForeColor = System.Drawing.Color.Black;
+            this.lblGuestID.Location = new System.Drawing.Point(160, 71);
+            this.lblGuestID.Name = "lblGuestID";
+            this.lblGuestID.Size = new System.Drawing.Size(31, 23);
+            this.lblGuestID.TabIndex = 2;
+            this.lblGuestID.Text = "????";
+            // 
+            // lblIsActive
+            // 
+            this.lblIsActive.BackColor = System.Drawing.Color.Transparent;
+            this.lblIsActive.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIsActive.ForeColor = System.Drawing.Color.Black;
+            this.lblIsActive.Location = new System.Drawing.Point(588, 71);
+            this.lblIsActive.Name = "lblIsActive";
+            this.lblIsActive.Size = new System.Drawing.Size(31, 23);
+            this.lblIsActive.TabIndex = 3;
+            this.lblIsActive.Text = "????";
+            // 
+            // ctrlGuestDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.Controls.Add(this.guna2GroupBox1);
+            this.Controls.Add(this.uCpersonCard1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ctrlGuestDetails";
+            this.Size = new System.Drawing.Size(1016, 546);
+            this.Load += new System.EventHandler(this.ctrlGuestDetails_Load);
+            this.guna2GroupBox1.ResumeLayout(false);
+            this.guna2GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private People.Controls.UCpersonCard uCpersonCard1;
+        private Guna.UI2.WinForms.Guna2GroupBox guna2GroupBox1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblIsActive;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblGuestID;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+    }
+}
---
+++ HMS/Guests/Controls/ctrlGuestDetails.cs
@@ -0,0 +1,49 @@
+﻿using GlobalClasses;
+using HMS_Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HMS.Guests.Controls
+{
+    public partial class ctrlGuestDetails : UserControl
+    {
+        int? GuestID;
+        public ctrlGuestDetails(int? guestID)
+        {
+            InitializeComponent();
+            GuestID = guestID;
+        }
+
+    
+
+        private void ctrlGuestDetails_Load(object sender, EventArgs e)
+        {
+            ClsGuest guest = ClsGuest.Find(GuestID.Value);
+            if (guest!=null)
+            {
+                uCpersonCard1._LoadPersonInfo(guest.PersonInfo.Id.Value);
+                lblGuestID.Text=guest.GuestID.ToString();
+                lblIsActive.Text = guest.IsActive ? "Yes" : "No";
+            }
+            else
+            {
+                ClsUtil.ShowErrorMessage("Guest Not Found Or An error Occoured!");
+                return;
+            }
+        }
+
+        public void Reset()
+        {
+            uCpersonCard1.Clear();
+            lblGuestID.Text = "????";
+            lblIsActive.Text = "????";
+        }
+    }
+}
