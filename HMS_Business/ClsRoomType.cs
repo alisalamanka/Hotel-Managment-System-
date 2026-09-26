b90ccc5ef50c553c8634a442157728b01266b9a4
abdo salamanka <aliachaaib50@gmail.com>
2026-09-26 17:49:53 +0000

    Room Type Class Added

---
+++ HMS_Business/ClsRoomType.cs
@@ -0,0 +1,100 @@
+﻿using GlobalClasses;
+using HMS_DataAccess;
+using System;
+using System.Data;
+
+namespace HMS_Business
+{
+    public class ClsRoomType
+    {
+        public int? RoomTypeID { get; set; }
+        public string RoomTypeName { get; set; }
+
+        public ClsRoomType()
+        {
+            RoomTypeID = null;
+            RoomTypeName = string.Empty;
+        }
+
+        public ClsRoomType(
+            int RoomTypeID,
+            string RoomTypeName)
+        {
+            this.RoomTypeID = RoomTypeID;
+            this.RoomTypeName = RoomTypeName;
+        }
+
+
+        public static ClsRoomType Find(int RoomTypeID)
+        {
+            string RoomTypeName = string.Empty;
+            Exception error = null;
+
+            bool Found =
+                ClsRoomTypeData.GetRoomTypeInfoByID(
+                    RoomTypeID,
+                    ref RoomTypeName,
+                    ref error);
+
+            if (!Found)
+            {
+                if (error != null)
+                {
+                    ClsUtil.ClsLogger.LogError(
+                        $"Failed to get Room Type Info with ID = {RoomTypeID}",
+                        error);
+                }
+
+                return null;
+            }
+
+            return new ClsRoomType(
+                RoomTypeID,
+                RoomTypeName);
+        }
+
+
+        public static DataTable GetAllRoomTypes()
+        {
+            Exception error = null;
+
+            DataTable dt =
+                ClsRoomTypeData.GetRoomTypesList(
+                    ref error);
+
+            if (error != null)
+            {
+                ClsUtil.ClsLogger.LogError(
+                    "Failed to get Room Types List",
+                    error);
+
+                return null;
+            }
+
+            return dt;
+        }
+
+
+        public bool UpdateRoomType()
+        {
+            Exception error = null;
+
+            bool Updated =
+                ClsRoomTypeData.UpdateRoomType(
+                    RoomTypeID.Value,
+                    RoomTypeName,
+                    ref error);
+
+            if (error != null)
+            {
+                ClsUtil.ClsLogger.LogError(
+                    $"Failed to Update Room Type with ID = {RoomTypeID.Value}",
+                    error);
+
+                return false;
+            }
+
+            return Updated;
+        }
+    }
+}
---
+++ HMS_DataAccess/ClsRoomTypeData.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HMS_DataAccess
+{
+    public class ClsRoomTypeData
+    {
+        public static bool GetRoomTypeInfoByID(
+            int RoomTypeID,
+            ref string RoomTypeName,
+            ref Exception ErrorOccurred)
+        {
+            string conn =
+                ConfigurationManager.AppSettings["ConnectionString"];
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(conn))
+                using (SqlCommand cmd = new SqlCommand(
+                    "SP_GetRoomTypeInfoByID", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue(
+                        "@RoomTypeID", RoomTypeID);
+
+                    cnn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            RoomTypeName =
+                                dr["RoomTypeName"].ToString();
+
+                            return true;
+                        }
+
+                        return false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred = e;
+                return false;
+            }
+        }
+
+
+        public static DataTable GetRoomTypesList(
+            ref Exception ErrorOccurred)
+        {
+            string conn =
+                ConfigurationManager.AppSettings["ConnectionString"];
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(conn))
+                using (SqlCommand cmd = new SqlCommand(
+                    "SP_GetRoomTypesList", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cnn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        DataTable dt = new DataTable();
+
+                        dt.Load(dr);
+
+                        return dt;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred = e;
+                return null;
+            }
+        }
+
+
+        public static bool UpdateRoomType(
+            int RoomTypeID,
+            string RoomTypeName,
+            ref Exception ErrorOccurred)
+        {
+            string conn =
+                ConfigurationManager.AppSettings["ConnectionString"];
+
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(conn))
+                using (SqlCommand cmd = new SqlCommand(
+                    "SP_UpdateRoomType", cnn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue(
+                        "@RoomTypeID", RoomTypeID);
+
+                    cmd.Parameters.AddWithValue(
+                        "@RoomTypeName", RoomTypeName);
+
+                    cnn.Open();
+
+                    int RowsAffected =
+                        Convert.ToInt32(cmd.ExecuteScalar());
+
+                    return RowsAffected > 0;
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorOccurred = e;
+                return false;
+            }
+        }
+    }
+}
